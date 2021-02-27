9cfda1e14f852b58f6cdcbc6dcdf5a0332bee9ee
SJGood96 <56788071+SJGood96@users.noreply.github.com>
2021-02-27 09:43:26 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+import { Route } from "react-router-dom"
+import './App.css';
+import NavBar from "./components/navigation_bar"
+import Footer from "./components/footer"
+import HomePage from './pages/home_page'
+import MinistriesPage from "./pages/ministries_page"
+import TeamsPage from "./pages/team_page"
+import FAQPage from "./pages/faq_page"
+import HistoryPage from "./pages/history_page"
+import TimesPage from "./pages/times_page"
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="container App">
+        <h1 className="text-center App-heading-text">Fremont Baptist Church</h1>
+        <NavBar />
+        <main>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/home' component={HomePage} />
+            <Route path='/ministries' component={MinistriesPage}/> 
+            <Route path='/history' component={HistoryPage}/>
+            <Route path='/team' component={TeamsPage}/>
+            <Route path='/times' component={TimesPage}/>
+            <Route path='/faq' component={FAQPage}/>
+          </Switch>
+          <Footer />
+        </main>
+        <br/>
+      </div>
+    );
+  }
+}
+ 
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  const developerName = 'Spencer Good'
+  return (
+    <div>
+      <hr/>
+      <p>Developer: {developerName}
+      <br/>
+      &copy; 2021</p>
+    </div>
+  );
+}
+
+export default Footer;
